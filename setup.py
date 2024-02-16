b368905a1cc34bec04f659e1779122f836f786fe
D-Jakobs <djakobs@ethz.ch>
2024-02-16 17:47:57 +0100

    update setup

--- setup.py
+++ setup.py
@@ -3,8 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name="HVAE",
     version="0.1",
+    url="https://github.com/D-Jakobs/HVAE/tree/AESR",
     description="Hierarchical Variational Autoencoder",
     author="Sebasitan Meznar",
+    author_email="example@gmail.com",
     packages=find_packages(),
     install_requires=[
         "numpy",
